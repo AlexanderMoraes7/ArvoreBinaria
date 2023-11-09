7e60b2be869156b33f2efa21d86b319ba50fd684
Alex <127699880+AlexanderMoraes7@users.noreply.github.com>
2023-11-09 16:39:51 -0300

    Update ExemploArvoreBinaria.java

--- ExemploArvoreBinaria.java
+++ ExemploArvoreBinaria.java
@@ -1,15 +1,17 @@
+package ArvoreBinaria;
+
 public class ExemploArvoreBinaria {
     public static void main(String[] args) {
-        ArvoreBinaria a = new ArvoreBinaria();
-        a.insere(10, "A");
-        a.insere(5, "B");
-        a.insere(15, "C");
-        a.insere(2, "D");
-        a.insere(7, "E");
-        a.insere(12, "F");
-        a.insere(6, "G");
-        a.insere(8, "H");
+        ArvoreBinaria a = new ArvoreBinaria(); // cria a árvore binária
+        a.insere(10,"A");
+        a.insere(5,"B");
+        a.insere(15,"C");
+        a.insere(2,"D");
+        a.insere(7,"E");
+        a.insere(12,"F");
+        a.insere(6,"G");
+        a.insere(8,"H");
         a.imprimeElementosArvore();
-        System.out.println("Altura : " + a.alturaArvore());
+        System.out.println("Altura: " + a.alturaArvore());
     }
 }
