e230be89fb2b742b10c37b60ebdb40ea14d9cd14
Alex <127699880+AlexanderMoraes7@users.noreply.github.com>
2023-11-13 15:28:03 -0300

    Update ExemploArvoreBinaria.java

--- ExemploArvoreBinaria.java
+++ ExemploArvoreBinaria.java
@@ -11,6 +11,11 @@ public class ExemploArvoreBinaria {
         a.insere(12,"F");
         a.insere(6,"G");
         a.insere(8,"H");
+        a.insere(1, "I");
+        a.insere(11, "J");
+        a.insere(20, "K");
+        a.insere(3, "L");
+        a.insere(13, "M");
         a.imprimeElementosArvore();
         System.out.println("Altura: " + a.alturaArvore());
     }
