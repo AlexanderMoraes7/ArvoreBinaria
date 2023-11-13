cc7a322bfc739b9795d336db30283576b8c002ff
Alex <127699880+AlexanderMoraes7@users.noreply.github.com>
2023-11-13 15:28:30 -0300

    Create ExemploABB.java

---
+++ ExemploABB.java
@@ -0,0 +1,24 @@
+package ArvoreBinaria;
+
+public class ExemploABB {
+    public static void main(String[] args) {
+        ArvoreBinaria a = new ArvoreBinaria(); // Cria a árvore binária de busca
+        a.insereABB(88,"elemento");
+        a.insereABB(70,"elemento");
+        a.insereABB(75,"elemento");
+        a.insereABB(99,"elemento");
+        a.insereABB(110,"elemento");
+        a.insereABB(105,"elemento");
+        a.insereABB(119,"elemento");
+        a.insereABB(80,"elemento");
+        a.insereABB(67,"elemento");
+        a.insereABB(59,"elemento");
+        a.insereABB(72,"elemento");
+        a.insereABB(91,"elemento");
+        a.insereABB(90,"elemento");
+        a.insereABB(95,"elemento");
+        a.insereABB(69,"elemento");
+        a.insereABB(77,"elemento adicional"); // um elemento adicional
+        a.imprimeElementosArvore();
+    }
+}
